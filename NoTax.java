490c5f4a0b6b30f81d1e2221738d51eede580969
Prabhav <116852836+Prabhav01@users.noreply.github.com>
2025-02-03 19:16:58 -0500

    Update NoTax.java

--- NoTax.java
+++ NoTax.java
@@ -1,4 +1,4 @@
-public class NoTax implements SalesTaxBehavior {
+public class NoTax implements SalesTaxBehavior {//for states with no tax rates such as Alaska
     @Override
     public double calc(double amount) {
         return 0.0;
